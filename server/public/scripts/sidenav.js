e5ad2b1a30d69ec9222e69ae4e35aceeba3f08d0
Pere Miquel Brull <peremiquelbrull@gmail.com>
2019-04-11 19:24:41 +0200

    Bootstrap sidebar init

--- server/public/scripts/sidenav.js
+++ server/public/scripts/sidenav.js
@@ -1,16 +1,7 @@
-$(function(){
+$(document).ready(function () {
 
-  $('.navigation .toggle-wrapper .show').on('click',function(){
-    $('.navigation').addClass('open');
-  });
-  $('.navigation .toggle-wrapper .hide').on('click',function(){
-    $('.navigation').removeClass('open');
-  });
-  $('.navigation .has-menu a').on('click',function(e){
-    e.stopPropagation();
-  });
-  $('.navigation .has-menu').on('click',function(){
-    $(this).toggleClass('open');
-  });
+    $('#sidebarCollapse').on('click', function () {
+        $('#sidebar').toggleClass('active');
+    });
 
 });
