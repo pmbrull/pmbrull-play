b0d3c0bde6454f73a05debc1029b901bdcf5b75a
Pere Miquel Brull <peremiquelbrull@gmail.com>
2019-05-10 19:27:17 +0200

    Fix getPostUrl

--- shared/src/main/scala/shared/Utils.scala
+++ shared/src/main/scala/shared/Utils.scala
@@ -5,7 +5,7 @@ object Utils {
   final val BASE_URL = "www.pmbrull.com"
   final val POSTS_URL = BASE_URL + "/posts"
 
-  def getPostUrl(post: Post): String = post.category + "/" + stringToUrl(post.title)
+  def getPostUrl(post: Post): String = stringToUrl(post.category) + "/" + stringToUrl(post.title)
 
   def stringToUrl(s: String): String = s.replaceAll(" ", "-").toLowerCase
 
